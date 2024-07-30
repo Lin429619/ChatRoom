7ac55497249564474fe175e5ff92fbe50321082a
lin <lin.04290619@gmail.com>
2024-07-30 21:58:03 +0800

    add

--- server.h
+++ server.h
@@ -15,6 +15,8 @@
 #include <thread>
 #include <vector>
 #include <mysql/mysql.h>
+#include <unordered_map>
+#include <pthread.h>
 using namespace std;
 
 class server {
@@ -23,12 +25,14 @@ class server {
         int server_sockfd;
         string server_ip;
         static vector<bool> sock_arr;
+        static unordered_map<string, int> name_sock_map; //名字和套接字描述符
+        static pthread_mutex_t name_sock_mutex; //互斥锁，锁住需要修改name_sock_map的临界区
     public:
         server(int port, string ip);
         ~server();
         void run();
         static void RecvMsg(int coon);
-        static void HandleRequest(int conn, string ip);
+        static void HandleRequest(int conn, string str, tuple<bool, string, string, int>&info);
         //待补充
 };
 
